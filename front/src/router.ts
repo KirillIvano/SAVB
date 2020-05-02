7b26d58a4baef3dba653ec469cf9a64e0cc55c77
KirillIvano <ivannadez@yandex.ru>
2020-05-03 01:25:11 +0300

    added not found page

--- front/src/router.ts
+++ front/src/router.ts
@@ -3,13 +3,18 @@ import VueRouter, {RouteConfig} from 'vue-router';
 import {
     VkAuthPage,
     VkAuthValidatePage,
+    NotFoundPage,
 } from './pages';
 
 const routes: RouteConfig[] = [
-    {path: '/validate_auth', component: VkAuthValidatePage},
     {path: '/vk_auth', component: VkAuthPage},
+    {path: '/validate_auth', component: VkAuthValidatePage},
+    {path: '*', component: NotFoundPage},
 ];
 
-export const router = new VueRouter({routes});
+export const router = new VueRouter({
+    mode: 'history',
+    routes,
+});
 
 

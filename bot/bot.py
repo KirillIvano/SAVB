8df9a6520f7252dc1cb45552bcfa6b874450deab
Gregory Kirillov <grishakirillov@mail.ru>
2020-05-06 15:06:56 +0300

    Code styling

--- bot/bot.py
+++ bot/bot.py
@@ -7,45 +7,50 @@ from keyboards import keyboard
 import json
 
 
-token = os.environ.get('vk_token')
-secret = os.environ.get('secret')
-confirmation_token = os.environ.get('confirmation_token')
+TOKEN = os.environ.get('VK_TOKEN')
+SECRET = os.environ.get('SECRET')
+CONFIRMATION_TOKEN = os.environ.get('CONFIRMATION_TOKEN')
 
-vk = vk_api.VkApi(token = token)
+vk = vk_api.VkApi(token = TOKEN)
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def home():
-    return "Welcome to main page. \n/newmessage to sent message to 169871363 user's id"
+    return "Welcome to main page. <br>/newmessage to sent message to 169871363 user's id"
 
 @app.route('/newmessage', methods=['GET'])
 def nm():
     data = {
-    "type": "message_new",
-    "object": {
-        "message": {
-            "from_id": 169871363,
-            "id": 147,
-            "text": "New user"
-        }
-    },
-    "secret": secret
+        "type": "message_new",
+        "object": {
+            "message": {
+                "from_id": 169871363,
+                "id": 147,
+                "text": "New user"
+            }
+        },
+        "secret": SECRET
     }
-    requests.post("http://194.67.109.99", data=json.dumps(data, ensure_ascii=False))
-    return 'Message was sent'
 
+    requests.post("http://194.67.109.99", data=json.dumps(data, ensure_ascii=False))
+
+    return 'Message was sent'
 
 @app.route('/', methods=['POST'])
 def messages():
     data = json.loads(request.data)
+
     try:
         if data['type'] == 'confirmation':
-            return confirmation_token
-        elif data['secret'] == secret:
+            return CONFIRMATION_TOKEN
+
+        elif data['secret'] == SECRET:
             if data['type'] == 'message_new':
                 id = data['object']['message']['from_id']
                 body = data['object']['message']['text']
+
                 vk.method("messages.send", {"peer_id": id, "message": body, "keyboard": keyboard, "random_id": 0})
+
                 return 'OK'
             else:
                 return 'I can only work with the message_new event'

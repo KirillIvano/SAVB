a3472ca98dbcbbd009bb61ee99032cc5852a4cfe
Gregory Kirillov <grishakirillov@mail.ru>
2020-05-13 17:48:58 +0300

    Update Library.py

--- bot/Library.py
+++ bot/Library.py
@@ -17,47 +17,41 @@ vk = vk_api.VkApi(token = token)
 method = "messages.getHistory"
 
 values = {}
-values['v'] = 5.92
+values['v'] = "5.92"
 values['access_token'] = token
-values['user_id'] = 169871363
 
-class lasttime():
-    time = 0
-    errors = 0
+info =  {}
+info['lasttime'] = 0
+info['errors'] = 0
 
-# создать элемент класса
-# абстрагироваться от статических данных
-# связать с bot.py
+async def get_function(session, method, params):
+    async with session.get("https://api.vk.com/method/" + method, params = params) as response:
+        data = await response.json()
+        try:
+            print(data['response']['count'])
+        except:
+            info['errors'] += 1
+            print(data)
 
-async def make_account(method, data):
+async def vk_method(method, params):
     async with aiohttp.ClientSession() as session:
-        lasttime.time = time.time() + 0.06
-        difference = lasttime.time - time.time() # Разница между текущим временем и БУДУЩИМ временем окончания запроса
-        lasttime.time += 0.06
-        if (difference > 0): # Если сейчас наступил момент времени окончания последнего запроса
-            await asyncio.sleep(difference) # Спим на время разницы прошлого запроса и текущего времени
-        async with session.get("https://api.vk.com/method/" + str(method), params=data) as response:
-            data = await response.json()
-            try:
-                print(data['response'])
-            except:
-                lasttime.errors += 1
-                print(data)
+        for i in range(100):
+            params.update(values)
+            difference = info['lasttime'] - time.time() 
+            if (difference > 0): 
+                info['lasttime'] += 0.06
+                await asyncio.sleep(difference) 
+            else:
+                info['lasttime'] = time.time() + 0.06
+            asyncio.gather(get_function(session, method, params))
 
-def vkmethod(method, data):
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(make_account(method, data))
-    print("Errors:", lasttime.errors)
 
-initial = time.time()
-vkmethod("messages.send", {"v": "5.92", "access_token": token, "user_id": 169871363, "random_id": 0, "message": "Test"})
-vkmethod("messages.send", {"v": "5.92", "access_token": token, "user_id": 169871363, "random_id": 0, "message": "Test"})
-vkmethod("messages.send", {"v": "5.92", "access_token": token, "user_id": 169871363, "random_id": 0, "message": "Test"})
-vkmethod("messages.send", {"v": "5.92", "access_token": token, "user_id": 169871363, "random_id": 0, "message": "Test"})
-vkmethod("messages.send", {"v": "5.92", "access_token": token, "user_id": 169871363, "random_id": 0, "message": "Test"})
-vkmethod("messages.send", {"v": "5.92", "access_token": token, "user_id": 169871363, "random_id": 0, "message": "Test"})
-vkmethod("messages.send", {"v": "5.92", "access_token": token, "user_id": 169871363, "random_id": 0, "message": "Test"})
-vkmethod("messages.send", {"v": "5.92", "access_token": token, "user_id": 169871363, "random_id": 0, "message": "Test"})
-vkmethod("messages.send", {"v": "5.92", "access_token": token, "user_id": 169871363, "random_id": 0, "message": "Test"})
-vkmethod("messages.send", {"v": "5.92", "access_token": token, "user_id": 169871363, "random_id": 0, "message": "Test"})
-print(time.time() - initial)
+
+start = time.time()
+
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(vk_method("messages.getHistory", {"user_id": 169871363})) # получение истории 
+
+# loop.run_until_complete(vk_method("messages.send", {"peer_id": 169871363, "random_id": 0, "message": "Test message"})) # отправка сообщения
+print(time.time() - start, "seconds passed, errors:", info['errors'])

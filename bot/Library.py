567fd05baebf9025f069c6e637f596e31b987022
Gregory Kirillov <kgn.0@ya.ru>
2020-05-14 22:06:16 +0300

    Update Library

--- bot/Library.py
+++ bot/Library.py
@@ -1,9 +1,6 @@
-import vk_api
 import os
 import dotenv
 import time
-import http
-import requests_async as requests
 import asyncio
 import traceback
 from aiohttp import ClientSession
@@ -12,9 +9,10 @@ import aiohttp
 dotenv.load_dotenv()
 token = os.environ.get('vk_token')
 
-vk = vk_api.VkApi(token = token)
-
-method = "messages.getHistory"
+methodsMap = {
+    'history': 'messages.getHistory',
+    'message': 'messages.send'
+}
 
 values = {}
 values['v'] = "5.92"
@@ -22,36 +20,69 @@ values['access_token'] = token
 
 info =  {}
 info['lasttime'] = 0
-info['errors'] = 0
 
-async def get_function(session, method, params):
-    async with session.get("https://api.vk.com/method/" + method, params = params) as response:
+def enchance_params(params: dict):
+    params.update(values)
+    return params
+    
+async def execute_method(
+    session: aiohttp.ClientSession,
+    method: str,
+    params: dict
+):
+    async with session.get(f"https://api.vk.com/method/{method}", params = enchance_params(params)) as response:
         data = await response.json()
-        try:
-            print(data['response']['count'])
-        except:
-            info['errors'] += 1
-            print(data)
+        print(data['response']['count'])
 
 async def vk_method(method, params):
     async with aiohttp.ClientSession() as session:
-        for i in range(100):
-            params.update(values)
-            difference = info['lasttime'] - time.time() 
-            if (difference > 0): 
-                info['lasttime'] += 0.06
-                await asyncio.sleep(difference) 
-            else:
-                info['lasttime'] = time.time() + 0.06
-            asyncio.gather(get_function(session, method, params))
+        difference = info['lasttime'] - time.time()
+        if (difference > 0): 
+            info['lasttime'] += 0.06
+            await asyncio.sleep(difference) 
+        else:
+            info['lasttime'] = time.time() + 0.06
 
-
+        # тут не было await, выполнение функции не ожидалось, и программа оканчивала свою работу до окончания запроса
+        await asyncio.gather(
+            execute_method(session, method, params) 
+        )
 
 start = time.time()
 
-
 loop = asyncio.get_event_loop()
-loop.run_until_complete(vk_method("messages.getHistory", {"user_id": 169871363})) # получение истории 
 
-# loop.run_until_complete(vk_method("messages.send", {"peer_id": 169871363, "random_id": 0, "message": "Test message"})) # отправка сообщения
-print(time.time() - start, "seconds passed, errors:", info['errors'])
+loop.run_until_complete(
+    vk_method(methodsMap['history'], {"user_id": 169871363})
+)
+loop.run_until_complete(
+    vk_method(methodsMap['history'], {"user_id": 169871363})
+)
+loop.run_until_complete(
+    vk_method(methodsMap['history'], {"user_id": 169871363})
+)
+loop.run_until_complete(
+    vk_method(methodsMap['history'], {"user_id": 169871363})
+)
+loop.run_until_complete(
+    vk_method(methodsMap['history'], {"user_id": 169871363})
+)
+loop.run_until_complete(
+    vk_method(methodsMap['history'], {"user_id": 169871363})
+)
+loop.run_until_complete(
+    vk_method(methodsMap['history'], {"user_id": 169871363})
+)
+loop.run_until_complete(
+    vk_method(methodsMap['history'], {"user_id": 169871363})
+)
+loop.run_until_complete(
+    vk_method(methodsMap['history'], {"user_id": 169871363})
+)
+loop.run_until_complete(
+    vk_method(methodsMap['history'], {"user_id": 169871363})
+)
+
+
+
+print(time.time() - start, "seconds passed")

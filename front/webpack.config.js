d05bef1dbad8692b2790e5187c7c30014a468678
KirillIvano <ivannadez@yandex.ru>
2020-05-15 22:58:55 +0300

    update configs

--- front/webpack.config.js
+++ front/webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
         },
     },
     output: {
-        path: path.resolve(__dirname, '..', '..', 'savb_testing', 'client'),
+        path: path.resolve(__dirname, 'dist'),
         publicPath: '/',
         filename: 'bundle.js'
     },

fee2f755f7362150f6e90e3f668b522e88aac9d4
KirillIvano <ivannadez@yandex.ru>
2020-05-16 23:43:23 +0300

    fixed popup

---
+++ front/src/parts/PopupBox/containers/withMessages.ts
@@ -0,0 +1,15 @@
+import {connect} from 'react-redux';
+import {Dispatch} from 'redux';
+
+import {RootState, RootAction} from '@/store/types';
+import {removePopupMessageStart} from '@/modules/popup/actions';
+
+const mapStateToProps = (state: RootState) => ({
+    messages: state.popupState.messages,
+});
+
+const mapDispatchToProps = (dispatch: Dispatch<RootAction>) => ({
+    removeMessage: (id: number) => dispatch(removePopupMessageStart(id)),
+});
+
+export const withMessages = connect(mapStateToProps, mapDispatchToProps);

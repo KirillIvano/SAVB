03fbf9f3c72f521468374884c9cbdac87f0fd71e
KirillIvano <ivannadez@yandex.ru>
2020-05-16 23:45:03 +0300

    remove useless stuff

---
+++ front/src/components/index.ts
@@ -0,0 +1,5 @@
+export {default as Card} from './Card';
+export {default as Button} from './Button';
+export {default as Tooltip} from './Tooltip';
+export {default as PageWrapper} from './PageWrapper';
+export {default as PageHeadline} from './PageHeadline';

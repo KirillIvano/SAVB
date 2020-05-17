514b7cd4d9f91a56c264e24d50f34ff021636ee3
KirillIvano <ivannadez@yandex.ru>
2020-05-17 23:21:33 +0300

    added header user info

--- front/src/parts/Header/components/HeaderUserInfo/index.tsx
+++ front/src/parts/Header/components/HeaderUserInfo/index.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import {Tooltip} from '@/components';
 
 import styles from './styles.less';
+import leaveIcon from './images/leave.svg';
 
 type HeaderUserInfoProps = {
     image: string;
@@ -14,9 +15,12 @@ const HeaderUserInfo: React.FC<HeaderUserInfoProps> = ({
     image,
     name,
 }) => (
-    <div className={styles.userInfo}>
-        <Tooltip src={image} size={'small'} />
-        <p className={styles.userName}>{name}</p>
+    <div className={styles.userInfoBlock}>
+        <div className={styles.userInfo}>
+            <Tooltip src={image} size={'small'} />
+            <p className={styles.userName}>{name}</p>
+        </div>
+        <img src={leaveIcon} className={styles.leaveIcon} alt="" />
     </div>
 );
 

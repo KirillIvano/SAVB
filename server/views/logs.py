7f1bf5238e9f2385fe189da9f7df60946195dd21
Elisa Wolia <liza11080@gmail.com>
2020-06-04 22:15:00 +0300

    files

---
+++ server/views/logs.py
@@ -0,0 +1,11 @@
+import asyncio
+from aiohttp import web
+from helpers.log import get_last_logs, logged
+from helpers.responses import generate_json_response
+
+routes = web.RouteTableDef()
+
+@routes.get('/logs')
+@logged(False)
+async def handle_logs(request: web.Request):
+	return generate_json_response(status=200, body=get_last_logs())

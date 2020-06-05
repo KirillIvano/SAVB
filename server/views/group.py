8622595449465f2eb10b4877f0e0e5d61324e840
artemmarkaryan <artemandriy@gmail.com>
2020-06-05 17:12:11 +0300

    user_id from cookies

--- server/views/group.py
+++ server/views/group.py
@@ -1,7 +1,7 @@
 # API routes for authorisation
 # Should start with /api/group
 from views.auth import check_auth
-from helpers import vk_api, cache, heavy_cache, responses
+from helpers import vk_api, cache, heavy_cache, responses, jwt
 from database.models import objects, Bot
 
 from aiohttp import web
@@ -12,16 +12,18 @@ routes = web.RouteTableDef()
 @check_auth
 async def handle(request: web.Request):
 
-	user_id: int = int(request.query.get('userId'))
+	cookies_body: dict = jwt.verify(request.cookies['accessJwt'])
+	user_id = cookies_body['userId']
+
 	if user_id is None:
-		return responses.generate_error_response('no user_id parameter', 401)
+		return responses.generate_error_response('no user_id parameter')
 
 	if cache.get_vk_token_cache().includes(user_id):
 		access_token = cache.get_vk_token_cache().get(user_id)
 	else:
 		access_token = await heavy_cache.get_vk_access_token(user_id)
 		if access_token is None:
-			return responses.generate_error_response('no access token in cache', 401)
+			return responses.generate_error_response('no access token in cache')
 
 	vk_response = await vk_api.group(access_token, user_id)
 	vk_response_body = vk_response.get('response')
@@ -39,7 +41,7 @@ async def handle(request: web.Request):
 				'id': group_id,
 				'name': group['name'],
 				'image': group['photo_200'],
-				'isUsed': len(in_db) > 0
+				'isUsed': len(in_db) > 0,
 			}
 		)
 

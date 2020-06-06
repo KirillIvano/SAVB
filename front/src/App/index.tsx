04114b8197f31413640c89f69ec36a73ad9f2ca4
KirillIvano <ivannadez@yandex.ru>
2020-06-06 18:12:09 +0300

    added new routes

--- front/src/App/index.tsx
+++ front/src/App/index.tsx
@@ -1,7 +1,12 @@
 import React, {useEffect} from 'react';
 import {Route, Switch} from 'react-router-dom';
 
-import {Bots, Main, UserAuth} from '@/pages';
+import {
+    Bots,
+    Main,
+    UserLoginPage,
+    BotCreatePage,
+} from '@/pages';
 import {PopupBox, Header} from '@/parts';
 import {withAuthRegulation} from '@/containers/withAuthRegulation';
 import {PageWrapper, Preloader} from '@/uikit';
@@ -32,7 +37,8 @@ const App = ({
                 <Switch>
                     <Route exact path="/" component={Main} />
                     <Route exact path="/bots" component={withAuthRegulation(Bots)} />
-                    <Route exact path="/userAuthPending" component={UserAuth} />
+                    <Route exact path="/userAuthPending" component={UserLoginPage} />
+                    <Route exact path="/groupAuthPending" component={BotCreatePage} />
                 </Switch>
             </PageWrapper>
             <PopupBox />
